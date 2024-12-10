5af56dd87f8b678edb73adc546612c58871e4587
blogchik <blogchikuz@gmail.com>
2024-12-10 21:18:30 +0500

    Default Config

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
     'app_name' => 'My Project',
-    'app_url' => 'https://devme.uz/dev/telegram-bots/cardinalucbot/public/index.php',
+    'app_url' => 'https://yourdomain.com/path/to/project/public/index.php',
     'environment' => $_ENV['APP_ENV'] ?? 'production',
     'debug' => filter_var($_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOLEAN),
     'timezone' => 'Asia/Tashkent',
